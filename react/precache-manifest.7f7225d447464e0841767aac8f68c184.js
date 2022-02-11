6fed314675097ca9543bad1d91384cc248844274
lurui <lurui@5eplay.com>
2022-02-11 16:13:09 +0800

    上传react

---
+++ react/precache-manifest.7f7225d447464e0841767aac8f68c184.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d32bee964f38189f6524b14f0c37790",
+    "url": "/index.html"
+  },
+  {
+    "revision": "240cb664113594ef787a",
+    "url": "/static/css/2.18061f9a.chunk.css"
+  },
+  {
+    "revision": "aa2ee6c86c1f28cc9ede",
+    "url": "/static/css/main.504a4117.chunk.css"
+  },
+  {
+    "revision": "240cb664113594ef787a",
+    "url": "/static/js/2.5ebb5563.chunk.js"
+  },
+  {
+    "revision": "e12f6fed8d9ed5cb105cebc4566e6e6f",
+    "url": "/static/js/2.5ebb5563.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aa2ee6c86c1f28cc9ede",
+    "url": "/static/js/main.f1e9ab22.chunk.js"
+  },
+  {
+    "revision": "3ec757ca8fb43ce546c9",
+    "url": "/static/js/runtime-main.68af30ad.js"
+  }
+]);
