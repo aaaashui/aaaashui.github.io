a01d6bcccfdf679ce4c546f00830153703cae4a3
lurui <lurui@5eplay.com>
2022-02-14 15:07:30 +0800

    提交文件

---
+++ react/precache-manifest.0bbbd3ab527b63272bc01fb63979c57b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "438bfe4e0177d570eb4e32b7c1967343",
+    "url": "./index.html"
+  },
+  {
+    "revision": "a5896c80f2721714e2a5",
+    "url": "./static/css/2.18061f9a.chunk.css"
+  },
+  {
+    "revision": "e6db77f0b13f0c305dfd",
+    "url": "./static/css/main.2fc4cb91.chunk.css"
+  },
+  {
+    "revision": "a5896c80f2721714e2a5",
+    "url": "./static/js/2.0a580c49.chunk.js"
+  },
+  {
+    "revision": "e12f6fed8d9ed5cb105cebc4566e6e6f",
+    "url": "./static/js/2.0a580c49.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e6db77f0b13f0c305dfd",
+    "url": "./static/js/main.5b761e93.chunk.js"
+  },
+  {
+    "revision": "08d0f69aac0dc97cc3f1",
+    "url": "./static/js/runtime-main.b129b25e.js"
+  },
+  {
+    "revision": "6bed5819080e5feff9fc21594bc711f3",
+    "url": "./static/media/Earth.6bed5819.png"
+  }
+]);
--- react/service-worker.js
+++ react/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.7f7225d447464e0841767aac8f68c184.js"
+  "./precache-manifest.0bbbd3ab527b63272bc01fb63979c57b.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
